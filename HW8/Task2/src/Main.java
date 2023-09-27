2554975c0bea05e287ac173696fe1c39b99b10a3
123456 <napnest@gmail.com>
2023-09-27 18:54:11 +0300

    Complete task 2

--- HW8/Task2/src/Main.java
+++ HW8/Task2/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Random;
+
 public class Main {
     public static void main(String[] args) {
-        //todo код программы писать тут
+        int[] people = new int[10];
+        int summa =0;
+        int count=0;
+        for (int i = 0; i < people.length ; i++) {
+            people[i]= new Random().nextInt(100-40)+40;
+            summa+=people[i];
+            if(people[i]>=60 && people[i]<=80){
+                count++;
+            }
+        }
+
+        for(int i:people){
+            System.out.print(i+" ");
+        }
+        System.out.println();
+        double average=(double)summa/10;
+        System.out.println("Средний вес составляет: "+average);
+        System.out.println("Количество людей с весом от 60 до 80 (включительно): "+count);
+
     }
 }
